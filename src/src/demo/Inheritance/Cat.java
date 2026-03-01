fd6c1fc6b44a4b24e5056c330ab91cf43d42079c
Phat Bui <phatphatrp@gmail.com>
2026-03-01 14:52:49 +0700

    Update Cat.java

--- src/src/demo/Inheritance/Cat.java
+++ src/src/demo/Inheritance/Cat.java
@@ -1,33 +1,19 @@
+
 package petmn;
 
-import java.util.Scanner;
 
-/**
- *
- * @author Admin
- */
 public class Cat extends Pet {
-
     private String furColor;
     private boolean isIndoor;
 
-    public Cat() {
-        super();
-    }
-
-    @Override
-    public void input() {
-        super.input();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("FurColor: ");
-        this.furColor = sc.nextLine();
-        System.out.println("Is Indoor? (true/false): ");
-        this.isIndoor = sc.nextBoolean();
+    public Cat(String id, String name, int age, double price, boolean isIndoor, String furColor) {
+        super(id, name, age, "Cat", price);
+        this.isIndoor = isIndoor;
+        this.furColor = furColor;
     }
 
     @Override
     public void display() {
-        super.display();
-        System.out.println("FurColor: " + this.furColor + "   Is Indoor: " + this.isIndoor);
+        System.out.println("Cat | Id: " + getId() +", Name: " + getName() +", Age: " + getAge() +", Price: " + getPrice() +", IsIndoor: " + isIndoor +", FurColor: " + furColor);
     }
 }
