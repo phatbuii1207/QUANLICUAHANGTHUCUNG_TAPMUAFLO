e1eb05d5b0cbeb33bdd2b2632da5ff16fec05838
Phat Bui <phatphatrp@gmail.com>
2026-03-01 14:52:07 +0700

    Update Pet.java

--- src/src/demo/Inheritance/Pet.java
+++ src/src/demo/Inheritance/Pet.java
@@ -1,54 +1,41 @@
 package petmn;
 
-import java.util.Scanner;
 
-public class Pet {
-
-    private String id;
+public abstract class Pet {
+    private  String id;
     private String name;
     private int age;
     private double price;
     private String type;
 
-    public Pet() {
-    }
-
-    public Pet(String id, String name, int age, double price, String type) {
+    public Pet(String id, String name, int age, String type, double price) {
         this.id = id;
         this.name = name;
         this.age = age;
-        this.price = price;
         this.type = type;
-    }
-
-    public void input() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("ID: ");
-        this.id = sc.nextLine();
-        System.out.println("Name: ");
-        this.name = sc.nextLine();
-        System.out.println("Age: ");
-        this.age = sc.nextInt();
-        System.out.println("Price: ");
-        this.price = sc.nextDouble();
-        sc.nextLine();
-        System.out.println("Type: ");
-        this.type = sc.nextLine();
-    }
-
-    public void display() {
-        System.out.println("ID: " + this.id + "   Name: " + this.name + "   Age: " + this.age + "   Price: " + this.price + "   Type: " + this.type);
-    }
-
-    public double getPrice() {
-        return this.price;
+        this.price = price;
     }
 
     public String getId() {
-        return this.id;
+        return id;
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getType() {
+        return type;
+    }
+    
+
+    public abstract void display();
 }
