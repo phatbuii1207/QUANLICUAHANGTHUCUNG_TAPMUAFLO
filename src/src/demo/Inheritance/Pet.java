ad8bfdcb05e6cc893c85b373d50cfceaa8fae511
tkbtuan <huynhtuan0357892@gmail.com>
2026-03-02 15:43:51 +0700

    Refactor Pet class to include input and display methods

--- src/src/demo/Inheritance/Pet.java
+++ src/src/demo/Inheritance/Pet.java
@@ -1,41 +1,54 @@
 package petmn;
 
+import java.util.Scanner;
 
-public abstract class Pet {
-    private  String id;
+public class Pet {
+
+    private String id;
     private String name;
     private int age;
     private double price;
     private String type;
 
-    public Pet(String id, String name, int age, String type, double price) {
+    public Pet() {
+    }
+
+    public Pet(String id, String name, int age, double price, String type) {
         this.id = id;
         this.name = name;
         this.age = age;
-        this.type = type;
         this.price = price;
+        this.type = type;
     }
 
-    public String getId() {
-        return id;
+    public void input() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ID: ");
+        this.id = sc.nextLine();
+        System.out.print("Name: ");
+        this.name = sc.nextLine();
+        System.out.print("Age: ");
+        this.age = sc.nextInt();
+        System.out.print("Price: ");
+        this.price = sc.nextDouble();
+        sc.nextLine();
+        System.out.print("Type: ");
+        this.type = sc.nextLine();
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
+    public void display() {
+        System.out.println("ID: " + this.id + " Name: " + this.name + " Age: " + this.age + " Price: " + this.price + " Type: " + this.type);
     }
 
     public double getPrice() {
-        return price;
+        return this.price;
     }
 
-    public String getType() {
-        return type;
+    public String getId() {
+        return this.id;
     }
-    
 
-    public abstract void display();
+    public String getName() {
+        return this.name;
+    }
 }
