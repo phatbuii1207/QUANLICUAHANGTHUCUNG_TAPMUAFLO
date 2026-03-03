fff9253d3c415e91d002831faa2dd5ccf01df302
Phat Bui <phatphatrp@gmail.com>
2026-03-03 07:37:49 +0700

    Update Dog.java

--- src/src/demo/Inheritance/Dog.java
+++ src/src/demo/Inheritance/Dog.java
@@ -1,17 +1,14 @@
-package petmn;
 
+package petmn;
 import java.util.Scanner;
 
-/**
- * * * @author Admin
- */
-public class Dog extends Pet {
-
+public class Dog extends Pet{
     private String breed;
     private boolean isTrained;
 
     public Dog() {
         super();
+        setType("Dog");
     }
     public Dog(String id, String name, int age, double price, boolean isTrained, String breed) {
         super(id, name, age, price, "Dog");
@@ -20,13 +17,12 @@ public class Dog extends Pet {
     }
 
     @Override
-    public void input() {
-        super.input();
-        Scanner sc = new Scanner(System.in);
+    public void input(Scanner sc) {
+        super.input(sc);
         System.out.println("Breed ");
         this.breed = sc.nextLine();
         System.out.println("Is Trained? (true/false): ");
-        this.isTrained = sc.nextBoolean();
+        this.isTrained = Boolean.parseBoolean(sc.nextLine());
     }
 
     @Override
@@ -34,4 +30,13 @@ public class Dog extends Pet {
         super.display();
         System.out.println("Breed: " + this.breed + " Is Trained? " + this.isTrained);
     }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public boolean isIsTrained() {
+        return isTrained;
+    }
+    
 }
