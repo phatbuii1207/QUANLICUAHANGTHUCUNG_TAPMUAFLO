5aebbdcc2959d023d613004f580b1c1af5e9c00d
Phat Bui <phatphatrp@gmail.com>
2026-03-03 07:42:36 +0700

    Update Pet.java

--- src/src/demo/Inheritance/Pet.java
+++ src/src/demo/Inheritance/Pet.java
@@ -1,9 +1,9 @@
-package petmn;
 
+package petmn;
 import java.util.Scanner;
 
 public class Pet {
-
+    
     private String id;
     private String name;
     private int age;
@@ -21,34 +21,45 @@ public class Pet {
         this.type = type;
     }
 
-    public void input() {
-        Scanner sc = new Scanner(System.in);
+    public void input(Scanner sc) {
+        
         System.out.print("ID: ");
         this.id = sc.nextLine();
         System.out.print("Name: ");
         this.name = sc.nextLine();
         System.out.print("Age: ");
-        this.age = sc.nextInt();
+        this.age = Integer.parseInt(sc.nextLine());
         System.out.print("Price: ");
-        this.price = sc.nextDouble();
-        sc.nextLine();
-        System.out.print("Type: ");
-        this.type = sc.nextLine();
+        this.price = Double.parseDouble(sc.nextLine());
     }
 
     public void display() {
         System.out.println("ID: " + this.id + " Name: " + this.name + " Age: " + this.age + " Price: " + this.price + " Type: " + this.type);
     }
 
-    public double getPrice() {
-        return this.price;
-    }
-
     public String getId() {
-        return this.id;
+        return id;
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    
 }
