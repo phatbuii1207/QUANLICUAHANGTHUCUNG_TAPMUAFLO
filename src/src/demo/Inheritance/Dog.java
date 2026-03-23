77f9ba012c6e9242d8551a189b0d2005dc4f7b0b
Phat Bui <phatphatrp@gmail.com>
2026-03-23 21:57:53 +0700

    LastCommit

--- src/src/demo/Inheritance/Dog.java
+++ src/src/demo/Inheritance/Dog.java
@@ -1,8 +1,11 @@
 
 package petmn;
+
 import java.util.Scanner;
 
-public class Dog extends Pet{
+
+public class Dog extends Pet implements ISellable{
+
     private String breed;
     private boolean isTrained;
 
@@ -10,6 +13,7 @@ public class Dog extends Pet{
         super();
         setType("Dog");
     }
+
     public Dog(String id, String name, int age, double price, boolean isTrained, String breed) {
         super(id, name, age, price, "Dog");
         this.isTrained = isTrained;
@@ -19,24 +23,33 @@ public class Dog extends Pet{
     @Override
     public void input(Scanner sc) {
         super.input(sc);
-        System.out.println("Breed ");
+        System.out.println("Breed: ");
         this.breed = sc.nextLine();
         System.out.println("Is Trained? (true/false): ");
         this.isTrained = Boolean.parseBoolean(sc.nextLine());
-    }
-
-    @Override
-    public void display() {
-        super.display();
-        System.out.println("Breed: " + this.breed + " Is Trained? " + this.isTrained);
     }
 
     public String getBreed() {
         return breed;
     }
 
-    public boolean isIsTrained() {
+    public boolean isTrained() {
         return isTrained;
     }
-    
+
+    @Override
+    public String toString() {
+
+        return super.toString() + "," + this.breed + "," + this.isTrained;
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Woof Woof ");
+    }
+
+    @Override
+    public double getDiscountPrice() {
+        return getPrice() * 1.1;
+    }
 }
